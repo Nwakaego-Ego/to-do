634d5b35f8eb01ff2be9d7e0aa83cc58bbf1ca8c
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-11-08 14:08:29 -0800

    added edit button

--- app/ArgumentsList/argumentsList.jsx
+++ app/ArgumentsList/argumentsList.jsx
@@ -1,16 +1,16 @@
 "use client";
+
 import React, { useState } from "react";
 
 function ArgumentsLength() {
   const [argumentCount, setArgumentCount] = useState(0);
   const [inputValue, setInputValue] = useState("");
   const [argumentsList, setArgumentsList] = useState([]);
-  const [selected, setSelected] = useState(-1);
 
   const handleAddArgument = () => {
     if (inputValue) {
       setArgumentsList([...argumentsList, inputValue]);
-      setArgumentCount(argumentCount + 1); // Increment the count directly.
+      setArgumentCount(argumentCount + 1);
       setInputValue("");
     }
   };
@@ -18,6 +18,12 @@ function ArgumentsLength() {
   function handleDelete(index) {
     const newList = argumentsList.filter((_, i) => i !== index);
     setArgumentsList(newList);
+  }
+
+  function handleUpdate(index, newValue) {
+    const updatedList = [...argumentsList];
+    updatedList[index] = newValue;
+    setArgumentsList(updatedList);
   }
 
   return (
@@ -33,7 +39,7 @@ function ArgumentsLength() {
 
         <button
           onClick={handleAddArgument}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow-md"
+          className="bg-blue-500 hover-bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow-md"
         >
           Add Argument
         </button>
@@ -44,10 +50,21 @@ function ArgumentsLength() {
           <li key={index}>
             {argument}{" "}
             <button
-              onClick={() => handleDelete(index)} // Pass the index to the delete function.
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow-md"
+              onClick={() => handleDelete(index)}
+              className="bg-blue-500 hover-bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow-md"
             >
               Delete
+            </button>
+            <button
+              onClick={() => {
+                const newValue = prompt("Enter a new value for the argument");
+                if (newValue !== null) {
+                  handleUpdate(index, newValue);
+                }
+              }}
+              className="bg-blue-500 hover-bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow-md"
+            >
+              Update
             </button>
           </li>
         ))}
